54db83398c153f658e7e5f14c947c4abcd1c0814
AAAActis <santiactis6@gmail.com>
2026-09-01 18:33:43 -0300

    feat(acceso): implementacion y configuracion del generador de token JWT

--- backend/LaCentral.Api/Program.cs
+++ backend/LaCentral.Api/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using LaCentral.Data;
 using LaCentral.Data.Models;
 using LaCentral.Data.Repositorios;
 using LaCentral.Data.Servicios; 
 using LaCentral.UseCases.Puertos;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using LaCentral.Api.Seguridad;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,9 +15,7 @@ builder.Services.AddDbContext<LaCentralDbContext>(options =>
 
 builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 builder.Services.AddScoped<IServicioHash, ServicioHash>();
-builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
-builder.Services.AddScoped<IServicioHash, ServicioHash>(); 
-builder.Services.AddScoped<IGeneradorToken, GeneradorToken>(); 
+builder.Services.AddScoped<IGeneradorToken, GeneradorToken>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
